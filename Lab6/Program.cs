dca5116c1565296eb4e849b9947c5dd423fffd5d
dawidgdowik1 <146650643+dawidgdowik1@users.noreply.github.com>
2026-01-17 22:05:32 +0100

    Update and rename Program.cs to Lab6/Program.cs

---
+++ Lab6/Program.cs
@@ -0,0 +1,46 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        IContactRepository repository = new TxtContactRepository();
+        List<Contact> contacts = repository.GetAll();
+
+        while (true)
+        {
+            Console.WriteLine("\n--- SYSTEM ZARZ¥DZANIA KONTAKTAMI ---");
+            Console.WriteLine("1. Wyœwietl wszystkie kontakty");
+            Console.WriteLine("2. Dodaj nowy kontakt");
+            Console.WriteLine("3. Zapisz i wyjdŸ");
+            Console.Write("Wybierz opcjê: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("\nLista kontaktów:");
+                    contacts.ForEach(c => Console.WriteLine(c));
+                    break;
+                case "2":
+                    Console.Write("Podaj ID: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj Imiê i Nazwisko: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Podaj Email: ");
+                    string email = Console.ReadLine();
+
+                    contacts.Add(new Contact { Id = id, Name = name, Email = email });
+                    Console.WriteLine("Dodano kontakt.");
+                    break;
+                case "3":
+                    repository.SaveAll(contacts);
+                    Console.WriteLine("Dane zapisane. Do widzenia!");
+                    return;
+                default:
+                    Console.WriteLine("Nieprawid³owa opcja.");
+                    break;
+            }
+        }
+    }
+}
